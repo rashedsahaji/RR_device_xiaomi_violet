7f59cda023dfeb683d690cfebd177551e3a13953
Rashed Sahaji <asifrashed2@gmail.com>
2020-01-20 20:02:20 +0530

    violet: remove RRO

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosip
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
